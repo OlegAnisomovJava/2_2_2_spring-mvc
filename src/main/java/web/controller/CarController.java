a7960cec0a92fcf53449dc070e48986f92a8e92f
OlegAnisimov <Ealex72@mail.ru>
2025-02-12 23:17:28 +0500

    commit spring-mvc_2_2_2

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import web.service.CarService;
+
+@Controller
+public class CarController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/cars")
+    public String showVievCars(Model model) {
+        model.addAttribute("cars", carService.getCars());
+        return "cars";
+    }
+}
